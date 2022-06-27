3fa5ba9b6b25ad88af0e7b1158bd3be04fd0d5c4
jpvSoccer <john.vogel123@gmail.com>
2022-06-27 19:06:34 -0400

    added new code and classes for initializing data file and parsing data file

--- DartWork/hackingAround/lib/src/classes.dart
+++ DartWork/hackingAround/lib/src/classes.dart
@@ -1,7 +1,71 @@
 // jvogel dart work
 // trying to put classes in a seperate file
 
+import "dart:io";
 import 'constants.dart';
+
+class MyFileParse {
+  File file;
+
+  MyFileParse(this.file);
+
+  myExtractDataFromFile() {
+    List parsedList = [];
+    print("Extracting data from file\n");
+    // String fileData = file.readAsStringSync();
+    List fileData = file.readAsLinesSync();
+    // remove comment lines
+    fileData.forEach((element) {
+      if (!(element.contains("#", 0))) {
+        parsedList.add(element);
+      }
+    });
+    print("data read from data file:\n$fileData \n\n");
+    //parsedList.remove(null);
+    print(parsedList);
+    //fileData.removeWhere((element) => false); not tested or used
+
+    //String parsedStringNoSpaces = fileData.replaceAll(' ', '');
+    //List parsedString = parsedStringNoSpaces.split(",");
+
+    //List parsedString = fileData.split(" ");
+    //print("parsed data read from data filei:\n$parsedString\n\n");
+    //parsedString.forEach(print);
+
+    //parsedString.forEach((element) => print(element));
+  }
+}
+
+class MyFileInitialization {
+  File file;
+  MyFileInitialization(this.file);
+
+  printTestFilePath() {
+    print(file.path);
+  }
+
+  removeOldTestFile() {
+    print("JPV removing data file");
+    Process.runSync('rm', ['../data/test.txt']);
+  }
+
+  myInitializeFile() {
+    if (file.existsSync() == false) {
+      print("\nAnimal data file does not exist; creating new data file\n");
+      print("Adding commented header to data file\n");
+      file.createSync();
+      file.writeAsStringSync("#Data file ussage\n", mode: FileMode.append);
+      file.writeAsStringSync("#first char # is comment line\n",
+          mode: FileMode.append);
+      file.writeAsStringSync("#here are the currently supported fields\n",
+          mode: FileMode.append);
+      file.writeAsStringSync("#animalId NAME DOB GENDER WEIGHT\n",
+          mode: FileMode.append);
+    } else {
+      print("Existing animal data file found\n");
+    }
+  }
+}
 
 // a class using the named default constructor
 class MyClass1 {
--- DartWork/hackingAround/lib/src/dartWork-1.dart
+++ DartWork/hackingAround/lib/src/dartWork-1.dart
@@ -1,37 +1,51 @@
 // jvogel dart work
-
+import 'dart:io';
 import 'constants.dart';
 import 'classes.dart';
 
+final file = File('../data/test.txt');
+
 void main() {
-  if (DEBUG1) print("$STRPREFIX in main method");
+  var my_file_initialization = new MyFileInitialization(file);
+  var my_file_parse = new MyFileParse(file);
+  //my_file_initialization.printTestFilePath();
+  //my_file_initialization.removeOldTestFile();
+  my_file_initialization.myInitializeFile();
+  my_file_parse.myExtractDataFromFile();
+
+//    file.writeAsStringSync("animalId, NAME, DOB, GENDER, WEIGHT\n",
+//        mode: FileMode.append);
+
+// this lets the program continue while file access is running
+//  Future<String> fileData = file.readAsString();
+//  fileData.then((c) => print(c));
 
 // making an instance of a class using default constructor
-  var my_class_1 = new MyClass1();
+//  var my_class_1 = new MyClass1();
 
 // making another instance
-  var my_class_1_a = new MyClass1();
+//  var my_class_1_a = new MyClass1();
 
 // overloading and calling class method
-  my_class_1_a.object_hello = "overloading instance variable";
-  my_class_1_a.printStuff();
+//  my_class_1_a.object_hello = "overloading instance variable";
+//  my_class_1_a.printStuff();
 
 // making an instance of a class with parameters in  constructor
 // parameters are nullable
-  var my_class_2 = new MyClass2(7, "instantiated");
-  my_class_2.printStuff();
+//  var my_class_2 = new MyClass2(7, "instantiated");
+//  my_class_2.printStuff();
 // overloading and calling class method
-  my_class_2.myInt = 1000;
-  my_class_2.printStuff();
+//  my_class_2.myInt = 1000;
+//  my_class_2.printStuff();
 
-  var my_class_2_a = new MyClass2(null, null);
-  my_class_2_a.printStuff();
-  my_class_2_a.myInt = 8;
-  my_class_2_a.object_hello = "hi from main";
-  my_class_2_a.printStuff();
+//  var my_class_2_a = new MyClass2(null, null);
+//  my_class_2_a.printStuff();
+//  my_class_2_a.myInt = 8;
+//  my_class_2_a.object_hello = "hi from main";
+//  my_class_2_a.printStuff();
 
 // try using named parameters
-  var my_class_3 = new MyClass3(object_hello: "testing named parameter");
+  // var my_class_3 = new MyClass3(object_hello: "testing named parameter");
 //  my_class_3.printStuff();
 //  my_class_3.myInt = 8;
 //  my_class_3.printStuff();
