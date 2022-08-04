738c7117e57f6882a96edaed91581aa27ec5429b
john vogel <john.vogel123@gmail.com>
2022-08-04 15:46:01 -0400

    enhanced the new flutter project script to kick off run as well

--- FlutterWork/NewProject.sh
+++ FlutterWork/NewProject.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 
 # Android SDK /home/jvogel/Android/Sdk
-# use AS device manager to create PIXEL emulator
+# Android cmd tools
+#/home/jvogel/Android/Sdk/cmdline-tools/latest/bin
+
+#screenshot2 screenshot.png
+# use AndroidStudio device manager to create PIXEL emulator
+#WARNING the command below does not make a device with enough memory
+#avdmanager delete avd -n jpv3
+#avdmanager create avd -n jpv3 -c 5G -k "system-images;android-33;google_apis;x86_64"
+#avdmanager list avd
+#avdmanager move avd -r myjpv
+#flutter emulators --launch jpv2
+#flutter devices
+#flutter run -d sdk
+#avdmanager list > devices.txt
+#avdmanager list target
+#avdmanager list device
+#avdmanager delete avd -n JPV1
 
 #flutter devices > Devices.dat
 #flutter emulators > Emulators.dat
@@ -15,16 +31,63 @@
 #flutter create --list-samples TemplateSamples.json
 #grep \"id\" TemplateSamples.json > ProjectSamples.dat
 
-#create a new project from a template
-flutter create ./singleChildScrollView \
---sample cupertino.SingleChildScrollView.2 \
---device-id Pixel_5_API_33 \
---description "jvogel first flutter app" \
---org net.secondchancelearning \
---project-name single_child_scroll_view \
---template=app
-#  $ cd singleChildScrollView
-#  $ flutter run
-#Your sample application code is in singleChildScrollView/lib/main.dart.
+export sampleName=singleChildScrollView
+export dirName=singleChildScrollView
+export projName=single_child_scroll_view
+export templateName=app
 
+export sampleName=cupertino.ListView.7
+export dirName=cupertinoListView
+export projName=cupertino_list_view
+
+# shows best practices for a real app
+export sampleName=none
+export templateName=skeleton
+export dirName=skeleton
+export projName=skeleton
+
+# very basic app for demo purposes
+export projectDescription="jvogel flutter app"
+export sampleName=material.Scaffold.1
+export templateName=app
+export dirName=sample
+export projName=sample
+export orgName=net.secondchancelearning
+
+# create  project to work with class programs
+# working script is copied from SourceSamples
+export dirName=educativeIoFlutterClass
+export projName=educative_io_flutter_class
+
+export deviceName=Pixel_5_API_33 
+export deviceName=Linux 
+
+echo "New project directory: $dirName"
+echo "Project description:   $projectDescription"
+echo "Project Name:          $projName"
+echo "Organization:          $orgName"
+echo "Project type:          $templateName"
+echo "Example source:        $sampleName"
+echo "Target device:         $deviceName"
+#exit
+
+#create a new project from a sample
+#--verbose \
+#FAILS --description $projectDescription \
+#PASS  --description "$projectDescription" \
+flutter create ./$dirName \
+--overwrite \
+--org $orgName \
+--project-name $projName \
+--description "$projectDescription" \
+--device-id $deviceName \
+--sample $sampleName \
+--template $templateName
+#exit
+
+# now run the new project
+cd $dirName
+flutter run -d $deviceName
+#flutter run
+#flutter run -d Crome
 
