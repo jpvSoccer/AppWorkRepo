f787b289afb4f329e190d26048b5dd1a457d81da
john vogel <john.vogel123@gmail.com>
2022-08-07 11:28:22 -0400

    created a new flutter script to be interactice, batch, produce skelton best practices, or basic up-counter sample

---
+++ Scripts/MakeFlutterProject.sh
@@ -0,0 +1,97 @@
+#!/bin/sh
+#jvogel
+# interactively or batch make a new flutter project using StdIn
+
+echo ""
+echo Usage: 
+echo MakeFlutterProject.sh -r batch
+echo MakeFlutterProject.sh -r interactive
+echo MakeFlutterProject.sh -r bestpractice
+echo MakeFlutterProject.sh -r basic
+echo ""
+
+while getopts r: flag
+do
+    case "${flag}" in
+        r) RunType=${OPTARG};;
+    esac
+done
+
+# these are defaults
+export orgName=net.secondchancelearning
+export deviceName=Pixel_5_API_33 
+export deviceName=Linux 
+
+if  [ $RunType = "batch" ] ; then
+  echo "Running in batch"
+  # very basic app for demo purposes
+  export projectDescription="jvogel basic increment app"
+  export sampleName=material.Scaffold.1
+  export templateName=app
+  export dirName=sample
+  export projName=sample
+elif [ $RunType = "bestpractice" ]; then
+  #Note: skeleton is a template typ, 
+  #      so you can't use --sample at the same time
+  #      so we skip the flutter run at the bottom
+  echo "Creating best practice sample"
+  export projectDescription="jvogel best practices app"
+  export templateName=skeleton
+  export dirName=best_practices
+  flutter create \
+	  --description "$projectDescription" \
+	  -t $templateName \
+	  $dirName
+  cd $dirName
+  flutter run -d $deviceName
+  exit
+elif [ $RunType = "basic" ]; then
+  echo "Creating basic upcounter sample"
+  export projectDescription="jvogel basic counter increment app"
+  export sampleName=material.Scaffold.1
+  export templateName=app
+  export dirName=counterSample
+  export projName=counter_sample
+elif [ $RunType = "interactive" ]; then
+  echo "Running interactive"
+  read  -p "Protect folder: " replyVar
+  export dirName="$replyVar"
+  read  -p "Project name: " replyVar
+  export projName="$replyVar"
+  read  -p "Project description: " replyVar
+  export projectDescription="$replyVar"
+  export sampleName=material.Scaffold.1
+  export templateName=app
+else
+  echo ""
+  echo "FATAL >> EXITING!!!"
+  echo "Run type not recognized: $RunType"
+  echo "Looking for either -r batch OR -r interactive"
+  echo ""
+  exit
+fi
+
+echo ""
+echo "Creating a new project:"
+echo "Project directory:     dirName $dirName"
+echo "Project description:   $projectDescription"
+echo "Project Name:          projName $projName"
+echo "Organization:          orgName  $orgName"
+echo "Project type:          templateName $templateName"
+echo "Example source:        sampleName $sampleName"
+echo "Target device:         deviceName $deviceName"
+echo ""
+
+#--verbose \
+flutter create ./$dirName \
+--overwrite \
+--org $orgName \
+--project-name $projName \
+--description "$projectDescription" \
+--device-id $deviceName \
+--sample $sampleName \
+--template $templateName
+
+cd $dirName
+flutter run -d $deviceName
+cd ..
