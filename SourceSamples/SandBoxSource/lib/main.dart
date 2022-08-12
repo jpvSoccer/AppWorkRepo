f7e097cbdad31d8982be2f7448d7d02b707e7cd9
jpvSoccer <john.vogel123@gmail.com>
2022-08-12 17:39:43 -0400

    added options for edicative io contact lesson

--- SourceSamples/SandBoxSource/lib/main.dart
+++ SourceSamples/SandBoxSource/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
-import 'classes/StatefulWidgetApp.dart';
-import 'classes/UpCounterApp.dart';
-import 'classes/HelloFlutterApp.dart';
+
+//import 'classes/my_app.dart';
+//import 'classes/stateful_widget_app.dart';
+import 'classes/contact_profile_page.dart';
+//import 'classes/up_counter_app.dart';
+//import 'classes/hello_flutter_app.dart';
 
 //void main() => runApp(MyApp());
-void main() => runApp(StatefulWidgetApp());
+//void main() => runApp(StatefulWidgetApp());
+void main() => runApp(ContactProfilePage());
+
 //void main() => runApp(UpCounterApp());
 //void main() => runApp(HelloFlutterApp());
