15629be05fee525125a25f7218c7cc9074b7ed37
john vogel <john.vogel123@gmail.com>
2022-08-14 13:37:11 -0400

    initial file updates for two-page set state example

--- SourceSamples/SandBoxSource/lib/main.dart
+++ SourceSamples/SandBoxSource/lib/main.dart
@@ -5,9 +5,11 @@ import 'package:flutter/material.dart';
 import 'classes/contact_profile_page.dart';
 //import 'classes/up_counter_app.dart';
 //import 'classes/hello_flutter_app.dart';
+//import 'classes/my_two_screens_home_page.dart:'
 
 //void main() => runApp(MyApp());
 //void main() => runApp(StatefulWidgetApp());
+//void main() => runApp(MyTwoScreensHomePage());
 void main() => runApp(ContactProfilePage());
 
 //void main() => runApp(UpCounterApp());
