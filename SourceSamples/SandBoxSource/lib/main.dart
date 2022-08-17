a4407167202591f368b71db29bbe17e0670efc06
jpvSoccer <john.vogel123@gmail.com>
2022-08-16 21:09:57 -0400

    reordered all lines to make it easier to run a particula sandbox app

--- SourceSamples/SandBoxSource/lib/main.dart
+++ SourceSamples/SandBoxSource/lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
 //import 'classes/my_app.dart';
-//import 'classes/stateful_widget_app.dart';
-import 'classes/contact_profile_page.dart';
-//import 'classes/up_counter_app.dart';
 //import 'classes/hello_flutter_app.dart';
+//import 'classes/up_counter_app.dart';
+//import 'classes/stateful_widget_app.dart';
 //import 'classes/my_two_screens_home_page.dart:'
+import 'classes/contact_profile_page.dart';
 
 //void main() => runApp(MyApp());
+//void main() => runApp(HelloFlutterApp());
+//void main() => runApp(UpCounterApp());
 //void main() => runApp(StatefulWidgetApp());
 //void main() => runApp(MyTwoScreensHomePage());
 void main() => runApp(ContactProfilePage());
 
-//void main() => runApp(UpCounterApp());
-//void main() => runApp(HelloFlutterApp());
