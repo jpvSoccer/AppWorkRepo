0e0ee9d3b9b0752e93b6e64b9c864c6e373462cc
jpvSoccer <john.vogel123@gmail.com>
2022-08-18 12:42:16 -0400

    put inports and main call together

--- SourceSamples/SandBoxSource/lib/main.dart
+++ SourceSamples/SandBoxSource/lib/main.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
 //import 'classes/my_app.dart';
-//import 'classes/hello_flutter_app.dart';
-//import 'classes/up_counter_app.dart';
-//import 'classes/stateful_widget_app.dart';
-//import 'classes/my_two_screens_home_page.dart:'
-import 'classes/contact_profile_page.dart';
-
 //void main() => runApp(MyApp());
+
+//import 'classes/hello_flutter_app.dart';
 //void main() => runApp(HelloFlutterApp());
+
+//import 'classes/up_counter_app.dart';
 //void main() => runApp(UpCounterApp());
+
+//import 'classes/stateful_widget_app.dart';
 //void main() => runApp(StatefulWidgetApp());
+
+//import 'classes/my_two_screens_home_page.dart:'
 //void main() => runApp(MyTwoScreensHomePage());
+
+import 'classes/contact_profile_page.dart';
 void main() => runApp(ContactProfilePage());
 
