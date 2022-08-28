abd15cf2c23935660745fed3b588ac3424c2aed9
john vogel <john.vogel123@gmail.com>
2022-08-28 11:06:12 -0400

    moved these sample classes out of the sandbox lib/classes folder because they were being read automatically by android studio even though they were not referenced in the contact page source and were causing a ton of errors

---
+++ SourceSamples/my_app.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import '../settings/constants.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  MyStateVar1 createState() => MyStateVar1();
+}
+
+class MyStateVar1 extends State<MyApp> {
+  MyStateVar1({Key? key});
+
+  String displayText = englishGreeting;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            appTitle,
+            style: const TextStyle(fontSize: 15),
+          ),
+          leading: const Icon(Icons.home),
+          actions: <Widget>[
+            IconButton(
+                iconSize: 25,
+                onPressed: () {
+                  setState(() {
+                    displayText = displayText == englishGreeting
+                        ? spanishGreeting
+                        : englishGreeting;
+                  });
+                },
+                icon: const Icon(Icons.refresh))
+          ],
+        ),
+        body: Center(
+          child: Text(displayText),
+        ),
+      ),
+    );
+  }
+}
---
+++ SourceSamples/stateful_widget_app.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import '../settings/constants.dart';
+
+class StatefulWidgetApp extends StatefulWidget {
+  const StatefulWidgetApp({Key? key}) : super(key: key);
+
+  @override
+  MyStateVar1 createState() => MyStateVar1();
+}
+
+class MyStateVar1 extends State<StatefulWidgetApp> {
+  MyStateVar1({Key? key});
+
+  String displayText = englishGreeting;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            appTitle,
+            style: const TextStyle(fontSize: 15),
+          ),
+          leading: const Icon(Icons.home),
+          actions: <Widget>[
+            IconButton(
+                iconSize: 25,
+                onPressed: () {
+                  setState(() {
+                    displayText = displayText == englishGreeting
+                        ? spanishGreeting
+                        : englishGreeting;
+                  });
+                },
+                icon: const Icon(Icons.refresh))
+          ],
+        ),
+        body: Center(
+          child: Text(
+            displayText,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+          ),
+        ),
+      ),
+    );
+  }
+}
