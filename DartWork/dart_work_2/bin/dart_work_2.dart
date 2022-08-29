4ecfe46129405937437a3bfba0e99a073216f6fa
jpvSoccer <john.vogel123@gmail.com>
2022-08-29 08:31:38 -0400

    removing old folders that are not applicable to the repo

--- DartWork/dart_work_2/bin/dart_work_2.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_work_2/dart_work_2.dart' as dart_work_2;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_work_2.calculate()}!');
-}
--- DartWork/dart_work_2/test/dart_work_2_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_work_2/dart_work_2.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
