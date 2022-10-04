5dbc01a6b36873a5efca7ebc084fc8a7229e34b8
john vogel <john.vogel123@gmail.com>
2022-10-04 18:15:31 -0400

    updated to reflect my growing knowledge about json and maps

--- SourceSamples/dartSamples/dartJsonHerdTest/lib/main.dart
+++ SourceSamples/dartSamples/dartJsonHerdTest/lib/main.dart
@@ -15,37 +15,40 @@ void main() {
   dynamic decodedHerdFileData = jsonDecode(herdFileString);
 
   List herdNamesList = [];
-  int herdCount = 0;
   List<dynamic> herd1MapList;
   List<dynamic> herd2MapList;
   List<dynamic> herd3MapList;
 
-  decodedHerdFileData.keys.forEach((item) {
+  int herdCount = 0;
+  // cycle through each herd
+  decodedHerdFileData.keys.forEach((herdName) {
+    //print("adding $herdName to top level keys list");
+    herdNamesList.add(herdName);
     ++herdCount;
-    //print("adding $item to top level keys list");
-    herdNamesList.add(item);
     int animalCount = 0;
     if (herdCount == 1) {
-      ++animalCount;
-      herd1MapList = decodedHerdFileData[item];
-      herd1MapList.forEach((item) {
-        //print(item);
-        print(item["id"]);
-        print(item["name"]);
-        print(item["dob"]);
-        print(item["weight"]);
-        print(item["vaccines"]);
-        print(item["notes"]);
+      herd1MapList = decodedHerdFileData[herdName];
+      herd1MapList.forEach((herdAnimalMap) {
+        ++animalCount;
+        print("-------------------------------");
+        //print(herdAnimalMap);
+        print(herdAnimalMap["id"]);
+        print(herdAnimalMap["name"]);
+        print(herdAnimalMap["dob"]);
+        print(herdAnimalMap["weight"]);
+        print(herdAnimalMap["vaccines"]);
+        print(herdAnimalMap["notes"]);
       });
-      //print("number of maps for $item is ${herd1MapList.length}");
+      //print("animal count for ${herdNamesList[0]} is $animalCount");
+      //print("number of maps in $herdName is ${herd1MapList.length}");
     }
     if (herdCount == 2) {
-      herd2MapList = decodedHerdFileData[item];
-      //print("number of maps  for $item is ${herd2MapList.length}");
+      herd2MapList = decodedHerdFileData[herdName];
+      //print("number of maps  for $herdName is ${herd2MapList.length}");
     }
     if (herdCount == 3) {
-      herd3MapList = decodedHerdFileData[item];
-      //print("number of maps  for $item is ${herd3MapList.length}");
+      herd3MapList = decodedHerdFileData[herdName];
+      //print("number of maps  for $herdName is ${herd3MapList.length}");
     }
     if (herdCount > SUPPORTEDNUMBEROFHERDS) {
       print("Error: top level object key count out of range!");
@@ -53,55 +56,7 @@ void main() {
     }
   });
   print("Found a total of $herdCount herd/s");
-  herdNamesList.forEach((item) {
-    print("Herd name found $item");
+  herdNamesList.forEach((herdName) {
+    print("Herd name found $herdName");
   });
-  exit(0);
-
-//  List<ANIMAL>? extractedAnimal;
-//  //read file contents
-//  var herdFileString = File(HERDJSONFILE).readAsStringSync();
-//  //print("\njson string from file read ${herdFileString}\n");
-//  //pass the read string to JsonDecoder
-//  final Map<String, dynamic> jsonmap = jsonDecode(herdFileString);
-//  //print("\nall maps after decoding herd file string ${jsonmap}\n");
-//
-//  //print("\nread string length ${jsonString.length}");
-//  //print("\nread string ${jsonString}");
-//
-//  //print("\nmap length ${jsonmap.length}\n");
-//  //print("\map entries ${jsonmap.entries}\n");
-//  print("\nherd map keys ${jsonmap.keys}\n");
-//  //print("\nherd map keys first ${jsonmap.keys.first}\n");
-//  //print("\herd nmap values ${jsonmap.values}\n");
-//  //print("\herd nmap values first ${jsonmap.values.first}\n");
-//
-//  //DataModel - key = "herd1", value = "ARRAY of Objects"
-//  String herdToExtract = "jsonObject0";
-//  print("\nextracting herd: ${herdToExtract}\n");
-//  var herdValues = jsonmap[herdToExtract];
-//  print("\nherd maps pulled from jsonmap ${herdValues}\n");
-//  if (herdValues != null) {
-//    extractedAnimal = <ANIMAL>[];
-//    //Each item in value is of type::: _InternalLinkedHashMap<String, dynamic>
-//    herdValues.forEach((item) {
-//      extractedAnimal?.add(new ANIMAL(
-//        id: item["id"],
-//        name: item["name"],
-//        dob: item["dob"],
-//        weight: item["weight"],
-//        vaccines: item["vaccines"],
-//        notes: item["notes"],
-//      ));
-//      //print("Animal Info: ${item}");
-//    });
-//  }
-//  //extractedAnimal?.forEach((element) => print(element));
-//  //print("\nextractedAnimal ${extractedAnimal}");
-//  print("\nextracted ${extractedAnimal?.length} animals");
-//  //print("\nMap 0 Animal 0 ${extractedAnimal?[0]}\n");
-//  //print("\nMap 1 Animal 1 ${extractedAnimal?[1]}\n");
-//  extractedAnimal?.forEach((item) {
-//    print("Animal Info: ${item}");
-//  });
 }
---
+++ SourceSamples/dartSamples/dartJsonHerdTest/lib/main_objectKeys.dart
@@ -0,0 +1,292 @@
+import 'dart:io';
+import 'dart:convert';
+
+/// This code will show how to access and decode a JSON
+/// file that has the following structure:
+/// a map with keys of topLevelKeys and values of a list-of-maps
+///   {
+///    "topLevelKey": [
+///        {
+///            "mapKey1": 0,
+///            "mapKey2": "value1",
+///        },
+///        {
+///            "mapKey1": "value5",
+///            "mapKey2": "value6"
+///        }
+///    ]
+///  }
+///
+
+const String JSONOBJECTSFILE = "sample_3_objects.json";
+//const String JSONOBJECTSFILE = "sample_1_object.json";
+const int SUPPORTEDNUMBEROFOBJECTS = 3;
+
+void main() {
+  // read json file into string
+  String? jsonFileString = File(JSONOBJECTSFILE).readAsStringSync();
+  //printJsonFileStringDebugger(jsonFileString);
+
+  // decode json string into top-level keys and maps
+  // keys go into topLevelKeysList
+  // maps go into extractedMapList0,1,2
+  dynamic decodedJsonFileData = jsonDecode(jsonFileString);
+  List topLevelKeysList = [];
+  int topLevelKeyCount = 0;
+  List<dynamic> extractedMapList0;
+  List<dynamic> extractedMapList1;
+  List<dynamic> extractedMapList2;
+  decodedJsonFileData.keys.forEach((item) {
+    ++topLevelKeyCount;
+    print("adding $item to top level keys list");
+    topLevelKeysList.add(item);
+    if (topLevelKeyCount == 1) {
+      extractedMapList0 = decodedJsonFileData[item];
+      print("number of maps for $item is ${extractedMapList0.length}");
+    }
+    if (topLevelKeyCount == 2) {
+      extractedMapList1 = decodedJsonFileData[item];
+      print("number of maps  for $item is ${extractedMapList1.length}");
+    }
+    if (topLevelKeyCount == 3) {
+      extractedMapList2 = decodedJsonFileData[item];
+      print("number of maps  for $item is ${extractedMapList2.length}");
+    }
+    if (topLevelKeyCount > SUPPORTEDNUMBEROFOBJECTS) {
+      print("Error: top level object key count out of range!");
+      print("Contact developer to add more capacity to APP");
+    }
+  });
+  exit(0);
+  //decodedJsonFileData.values.forEach((item) => print("found value $item"));
+  //printJsonObjectsDebugger(decodedJsonFileData);
+  // create a list that will contain all first level keys found in json file
+  //for (var objectKey in decodedJsonFileData.keys) {
+  //  topLevelKeysList.add(objectKey);
+  //}
+  //print("objects: $topLevelKeysList");
+  //print("object 0: ${topLevelKeysList[0]}");
+  //print("object 1: ${topLevelKeysList[1]}");
+  //print("object 2: ${topLevelKeysList[2]}");
+  //topLevelKeysList.insert(3, "myNewObject");
+  //print("topLevelKeysList: ${topLevelKeysList.elementAt(0)}");
+
+  //iterate through the json top-level object key list
+  // and extract the associated maps for each key
+  int objectCount = 0;
+  for (String topLevelKey in topLevelKeysList) {
+    ++objectCount;
+    if (objectCount == 1) {
+      List<dynamic> extractedMapList0 = decodedJsonFileData[topLevelKey];
+      //print("$topLevelKey $extractedMapList");
+      print("\nobject $topLevelKey");
+      print("number of maps ${extractedMapList0.length}");
+      int mapCount = 0;
+      for (dynamic item in extractedMapList0) {
+        ++mapCount;
+        //print("map $mapCount $item");
+      }
+      //print("map 0 from $topLevelKey is ${extractedMapList0[0]}");
+      if (mapCount == 1) {
+        Map map0 = extractedMapList0[0];
+        print("number of k-v pairs ${map0.length}");
+        print({
+          map0.keys.elementAt(objectCount - 1),
+          map0.values.elementAt(objectCount - 1)
+        });
+        print({
+          map0.keys.elementAt(objectCount - 0),
+          map0.values.elementAt(objectCount - 0)
+        });
+      }
+      //extractedMapList1
+      //    .forEach((item) => print("Extracted map from $topLevelKey $item"));
+    }
+    if (objectCount == 2) {
+      List<dynamic> extractedMapList1 = decodedJsonFileData[topLevelKey];
+      print("\nobject $topLevelKey");
+      print("number of maps ${extractedMapList1.length}");
+      int mapCount = 0;
+      for (dynamic item in extractedMapList1) {
+        ++mapCount;
+        //  print("map $mapCount $item");
+      }
+//      if (mapCount == 1) {
+//        Map map0 = extractedMapList1[0];
+//        print("number of k-v pairs ${map0.length}");
+//        print({
+//          map0.keys.elementAt(objectCount - 1),
+//          map0.values.elementAt(objectCount - 1)
+//        });
+//        print({
+//          map0.keys.elementAt(objectCount - 0),
+//          map0.values.elementAt(objectCount - 0)
+//        });
+//      }
+      if (mapCount == 2) {
+        Map map1 = extractedMapList1[0];
+        Map map2 = extractedMapList1[1];
+        print("map 1 number of k-v pairs ${map1.length}");
+        print({
+          map1.keys.elementAt(objectCount - 2),
+          map1.values.elementAt(objectCount - 2)
+        });
+        print({
+          map1.keys.elementAt(objectCount - 1),
+          map1.values.elementAt(objectCount - 1)
+        });
+        print({
+          map1.keys.elementAt(objectCount - 0),
+          map1.values.elementAt(objectCount - 0)
+        });
+        print("map 2 number of k-v pairs ${map2.length}");
+        print({
+          map2.keys.elementAt(objectCount - 2),
+          map2.values.elementAt(objectCount - 2)
+        });
+        print({
+          map2.keys.elementAt(objectCount - 1),
+          map2.values.elementAt(objectCount - 1)
+        });
+        print({
+          map2.keys.elementAt(objectCount - 0),
+          map2.values.elementAt(objectCount - 0)
+        });
+      }
+      //print("map 0 from $topLevelKey is ${extractedMapList2[0]}");
+      //print("map 1 from $topLevelKey is ${extractedMapList2[1]}");
+      //extractedMapList2
+      //    .forEach((item) => print("Extracted map from $topLevelKey $item"));
+    }
+    ;
+    if (objectCount == 3) {
+      List<dynamic> extractedMapList2 = decodedJsonFileData[topLevelKey];
+      //print("$topLevelKey $extractedMapList2");
+      print("\nobject $topLevelKey");
+      print("number of maps ${extractedMapList2.length}");
+      int mapCount = 0;
+      for (dynamic item in extractedMapList2) {
+        ++mapCount;
+        //print("map $mapCount $item");
+      }
+      if (mapCount == 3) {
+        Map map0 = extractedMapList2[0];
+        Map map1 = extractedMapList2[1];
+        Map map2 = extractedMapList2[2];
+        print("map0 number of k-v pairs ${map0.length}");
+        print({
+          map0.keys.elementAt(objectCount - 3),
+          map0.values.elementAt(objectCount - 3)
+        });
+        print({
+          map0.keys.elementAt(objectCount - 2),
+          map0.values.elementAt(objectCount - 2)
+        });
+        print({
+          map0.keys.elementAt(objectCount - 1),
+          map0.values.elementAt(objectCount - 1)
+        });
+        print({
+          map0.keys.elementAt(objectCount - 0),
+          map0.values.elementAt(objectCount - 0)
+        });
+        print("map1 number of k-v pairs ${map1.length}");
+        print({
+          map1.keys.elementAt(objectCount - 3),
+          map1.values.elementAt(objectCount - 3)
+        });
+        print({
+          map1.keys.elementAt(objectCount - 2),
+          map1.values.elementAt(objectCount - 2)
+        });
+        print({
+          map1.keys.elementAt(objectCount - 1),
+          map1.values.elementAt(objectCount - 1)
+        });
+        print({
+          map1.keys.elementAt(objectCount - 0),
+          map1.values.elementAt(objectCount - 0)
+        });
+        print("map2 number of k-v pairs ${map2.length}");
+        print({
+          map2.keys.elementAt(objectCount - 3),
+          map2.values.elementAt(objectCount - 3)
+        });
+        print({
+          map2.keys.elementAt(objectCount - 2),
+          map2.values.elementAt(objectCount - 2)
+        });
+        print({
+          map2.keys.elementAt(objectCount - 1),
+          map2.values.elementAt(objectCount - 1)
+        });
+        print({
+          map2.keys.elementAt(objectCount - 0),
+          map2.values.elementAt(objectCount - 0)
+        });
+      }
+      //print("map 0 from $topLevelKey is ${extractedMapList2[0]}");
+      //print("map 1 from $topLevelKey is ${extractedMapList2[1]}");
+      //print("map 2 from $topLevelKey is ${extractedMapList2[2]}");
+      //extractedMapList2
+      //    .forEach((item) => print("Extracted map from $topLevelKey $item"));
+    }
+    ;
+    if (objectCount == 4) {
+      print(
+          "WARNING: there is no 4th json object by the name of ${topLevelKeysList.elementAt(objectCount - 1)}");
+    }
+    ;
+  }
+  exit(0);
+//JPV  // extract all relevant objects
+//JPV  //// sample json has three objects that can be extracted
+//JPV  //   listOfMaps1, listOfMaps2, listOfMaps3
+//JPV//  dynamic extractedMap1 = decodedJsonFileData["listOfMaps1"];
+//JPV  dynamic extractedMap2 = decodedJsonFileData["listOfMaps2"];
+//JPV  dynamic extractedMap3 = decodedJsonFileData["listOfMaps3"];
+//JPV  //printExtractedObjectsToManipulateDebugger(
+//JPV  //    extractedMap1, extractedMap2, extractedMap3);
+//JPV
+//JPV  int mapCount = 0;
+//JPV  if (extractedMap1 != null) {
+//JPV    extractedMap1.forEach((item) {
+//JPV      //print(decodedJsonFileData.keys.first);
+//JPV      ++mapCount;
+//JPV      //print("\nExtracted Object Item: ${item}");
+//JPV//      print("${item}  map ${mapCount} keys: ${item.keys}");
+//JPV//      print("$item  map ${mapCount} values: ${item.values}");
+//JPV      //print("mapKey1 value: ${item["mapKey1"]}");
+//JPV      //print("mapKey2 value: ${item["mapKey2"]}\n");
+//JPV    });
+//JPV  }
+//JPV  //printExtractedObjectDebugger(formattedObject1);
+//JPV  mapCount = 0;
+//JPV  if (extractedMap2 != null) {
+//JPV    ++mapCount;
+//JPV    extractedMap2.forEach((item) {
+//JPV      //     print("\nExtracted Object Item: ${item}");
+//JPV      print("$item  map ${mapCount} keys: ${item.keys}");
+//JPV      print("$item  map ${mapCount} values: ${item.values}");
+//JPV      //print("mapKey1 value: ${item["mapKey1"]}");
+//JPV      //print("mapKey2 value: ${item["mapKey2"]}");
+//JPV      //print("mapKey3 value: ${item["mapKey3"]}\n");
+//JPV    });
+//JPV  }
+//JPV  //printExtractedObjectDebugger(formattedObject2);
+//JPV
+//JPV  mapCount = 0;
+//JPV  if (extractedMap3 != null) {
+//JPV    ++mapCount;
+//JPV    extractedMap3.forEach((item) {
+//JPV//      print("\nExtracted Object Item: ${item}");
+//JPV      print("$item  map ${mapCount} keys: ${item.keys}");
+//JPV      print("$item  map ${mapCount} values: ${item.values}");
+//JPV      //print("mapKey1 value: ${item["mapKey1"]}");
+//JPV      //print("mapKey2 value: ${item["mapKey2"]}");
+//JPV      //print("mapKey3 value: ${item["mapKey3"]}");
+//JPV      //print("mapKey4 value: ${item["mapKey4"]}\n");
+//JPV    });
+//JPV  }
+//JPV  //printExtractedObjectDebugger(formattedObject2);
+}
