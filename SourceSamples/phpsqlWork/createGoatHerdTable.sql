18fa5b964387aa7ca9e07be4c44f1bde4002f7ca
jvogel <john.vogel123@gmail.com>
2023-01-25 12:08:52 -0500

    added not on how to alter value

--- SourceSamples/phpsqlWork/createGoatHerdTable.sql
+++ SourceSamples/phpsqlWork/createGoatHerdTable.sql
@@ -9,3 +9,4 @@ CREATE TABLE `GoatHerd` (
   `notes` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+#ALTER TABLE `GoatHerd` CHANGE `breed` `breed` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'goatBreed';
