f15aa409d7066a17f7a5cd382479ba9dd5c251a1
jvogel <john.vogel123@gmail.com>
2023-01-25 12:05:23 -0500

    i need to be root to do this

--- SourceSamples/phpsqlWork/createDb.sql
+++ SourceSamples/phpsqlWork/createDb.sql
@@ -1,1 +1,8 @@
+#for some reason jvogel cannot create databases
+#so we need to use root msql command line to create a database
+#and then grant privilages to jvogel
+
+sudo mysql -u root -p
 CREATE DATABASE goatDb;
+GRANT ALL PRIVILEGES ON goatDb.* TO 'jvogel'@'localhost';
+SHOW GRANTS FOR 'jvogel'@'localhost';
